ccb520794052d1dd0d8e2dc2e24ef36dcc045c83
Brett Lyons <blyons@students.naropa.edu>
2017-06-03 10:08:11 -0600

    General cleanup, removed TODO entries, added maintainer lines

--- cwk_installer.sh
+++ cwk_installer.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
-# Master Script that runs the key mappings and the installation script
+# Maintainer: Brett Lyons <blyons@students.naropa.edu>
+# Contributor: Etienne Begert <william.begert@codingwithkids.com>
+# Github: https://www.github.com/brettlyons/cwk_scripts
 
-echo 'Creating temporary directory for downloaded files'
-cd "$HOME" || return  # we want to start in the same place every time
+cd "$HOME" || return  # start in the same place every time.
 mkdir temp
-mv install.sh temp
+mv $0 temp
 cd temp || return
 
-# TODO: factor out desktop icons into a script separate from installs.
-# so we don't run the desktop icon setting up with sudo permissions
-
-echo 'Installing updates and programs'
 curl -O programs.sh https://raw.githubusercontent.com/brettlyons/cwk_scripts/master/programs.sh
-
 curl -O programs.sh https://raw.githubusercontent.com/brettlyons/cwk_scripts/master/desktop_setup.sh
 
 sudo bash programs.sh
 bash desktop_setup.sh
 
 cd "$HOME" || return
-echo 'Removing temporary files'
 rm -r -f temp
 
 shutdown -r -t 5
